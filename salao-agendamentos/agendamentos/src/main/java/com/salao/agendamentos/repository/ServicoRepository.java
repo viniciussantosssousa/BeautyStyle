346102306dd0b55b64409459079d7d6215c8eb6f
LucasHNascimento <130712881+LucasHNascimento@users.noreply.github.com>
2025-06-04 16:21:21 -0300

    Delete salao-agendamentos directory

--- salao-agendamentos/agendamentos/src/main/java/com/salao/agendamentos/repository/ServicoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.salao.agendamentos.repository;
-
-import com.salao.agendamentos.model.Servico;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServicoRepository extends JpaRepository<Servico, Long> {
-}
